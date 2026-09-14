58c816771cdb903abb92118f461e7e3336ab1f4f
os233 <126555186+os233@users.noreply.github.com>
2026-09-15 04:20:35 +0800

    release: 发布 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.example.waterreminder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.3.1"
+        versionCode = 7
+        versionName = "1.4.0"
     }
 
     signingConfigs {
